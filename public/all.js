0be25122f596354248bcb9cd722ec9603eedf417
Nuxnuxx <nuxnux02290@gmail.com>
2022-02-24 00:52:25 +0100

    Add visual data to analytics (only symbol)

--- public/all.js
+++ public/all.js
@@ -1,12 +1,16 @@
 async function getData(){
 	const response = await fetch('/api')
 	const data = await response.json()
+	const table = document.getElementById('myTable');
+	let i = 0;
 	for(items of data){
-		const root = document.createElement('div');
-		const geo = document.createElement('div');
-		geo.textContent = `${items.latitude}°, ${items.longitude}°`
-		root.append(geo)
-		document.body.append(root)
+		var row = table.insertRow(i+1)
+		var cell1 = row.insertCell(0)
+		var cell2 = row.insertCell(1)
+
+		cell1.innerHTML = i+1
+		cell2.innerHTML = items.symbol
+		i++
 	}
 	console.log(data)
 }
