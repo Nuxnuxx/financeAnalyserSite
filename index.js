d19e4c60563fed24a3ceb9ec3eb741a98202bb99
Nuxnuxx <nuxnux02290@gmail.com>
2022-02-24 01:05:32 +0100

    make hosting possible with port

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express')
 const Datastore = require('nedb')
 const fetch = require('node-fetch')
 const app = express()
+const port = process.env.PORT || 3000
 
 // START OF SERVER
-app.listen(3000,() => console.log('listening at 3000'))
+app.listen(port,() => console.log(`listening at ${port}`))
 app.use(express.static('public'))
 app.use(express.json({limit: '1mb'}))
 
